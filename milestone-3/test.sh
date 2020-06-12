a723dd4cafced6953cca255367c70d2923f5f18a
Michal Rutka <michal.rutka@gmail.com>
2020-06-12 20:19:28 +0200

    Milestone-3 run on minikube

---
+++ milestone-3/test.sh
@@ -0,0 +1,12 @@
+# kubectl apply -f namespace.yml -f mysql.yml -f app.yml
+# kubectl expose deployment profile --type=NodePort -n m2k8s
+URL=$(minikube service profile --url -n m2k8s)
+echo $URL
+echo "-> http POST $URL/profile username=unamerkel password=changeme firstName=Una lastName=Merkel email=unamerkel@example.com"
+http POST $URL/profile username=unamerkel password=changeme firstName=Una lastName=Merkel email=unamerkel@example.com
+echo "-> http GET $URL/profile/unamerkel"
+http GET $URL/profile/unamerkel
+echo "-> http -f POST $URL/profile/unamerkel/image file@una_merkel.jpg"
+http -f POST $URL/profile/unamerkel/image file@una_merkel.jpg
+echo "-> http -f GET $URL/profile/unamerkel/image file@una_merkel.jpg"
+http -f GET $URL/profile/unamerkel/image file@una_merkel.jpg
