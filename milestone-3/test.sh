2b5cbca02ffaa6f50c07710d153fa5d04353d3f6
Michal Rutka <michal.rutka@gmail.com>
2020-06-13 11:52:08 +0200

    secrets added

--- milestone-3/test.sh
+++ milestone-3/test.sh
@@ -1,6 +1,9 @@
+NAMESPACE=m2k8s
 # kubectl apply -f namespace.yml -f mysql.yml -f app.yml
-# kubectl expose deployment profile --type=NodePort -n m2k8s
-URL=$(minikube service profile --url -n m2k8s)
+# kubectl expose deployment profile --type=NodePort -n $NAMESPACE
+# kubectl config set-context --current --namespace $NAMESPACE
+# kubectl exec mypod -it sh
+URL=$(minikube service profile --url -n $NAMESPACE)
 echo $URL
 echo "-> http POST $URL/profile username=unamerkel password=changeme firstName=Una lastName=Merkel email=unamerkel@example.com"
 http POST $URL/profile username=unamerkel password=changeme firstName=Una lastName=Merkel email=unamerkel@example.com
